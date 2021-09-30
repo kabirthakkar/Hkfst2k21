9aaaa50e7189fca1a46ac07753083360661f8b39
Achyut Tripathi <62375358+Achyut08@users.noreply.github.com>
2021-10-01 02:24:19 +0530

    Add files via upload

---
+++ ASCII Value.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int ascii;
+	char ch;
+	printf("To print ascii value of a character");
+	printf("\nenter the character");
+	scanf("%c",&ch);
+	printf("\nThe value of ascii is %d",ch);
+	return 0;
+}
