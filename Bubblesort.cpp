365edee0c6f911888a9310903ab4f6e129970e09
Nishant Shaklan <nishantshaklan@Nishants-MacBook-Air.local>
2021-10-01 11:15:56 +0530

    Added Bubblesort.cpp

--- Bubblesort.cpp
+++ Bubblesort.cpp
@@ -3,14 +3,16 @@ using namespace std;
 
 int main(){
     int n;
+    cout<<"Enter the size of arry :";
     cin>>n;
-
+    
+    cout<<"Enter Array Elements :";
     int arr[n];
     for (int i = 0; i < n; i++)
     {
         cin>>arr[i];
     }
-
+    
     int counter =1;
     while (counter<n){    
     for(int i=0; i<n-counter; i++){
@@ -22,11 +24,13 @@ int main(){
     }
     counter++;
     }
-
+    
+    cout<<"Array after Sorting :";
     for (int i = 0; i < n; i++)
     {
         cout<<arr[i]<<" ";
     }cout<<endl;
+    
     
     
 }
