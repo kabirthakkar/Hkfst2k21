c29ce6b5320efe96372985162226d8f34e93f945
Ishh1505 <93137470+Ishh1505@users.noreply.github.com>
2021-10-26 20:27:18 +0530

    created reverse_string.c
    A C program to reverse the entered string

---
+++ reverse_string.c
@@ -0,0 +1,26 @@
+//A C program to reverse the entered string
+
+#include <stdio.h>
+#include <string.h>
+ 
+int main()
+{
+  	char Str[100], RevStr[100];
+  	int i, j, len;
+ 
+  	printf("\n Please Enter any String :  ");
+  	gets(Str);
+  	
+  	j = 0;
+  	len = strlen(Str);
+ 
+  	for (i = len - 1; i >= 0; i--)
+  	{
+  		RevStr[j++] = Str[i];
+  	}
+  	RevStr[i] = '\0';
+ 
+  	printf("\n String after Reversing = %s", RevStr);
+  	
+  	return 0;
+}
